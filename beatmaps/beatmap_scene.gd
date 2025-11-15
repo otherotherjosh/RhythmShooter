2b717908d5e8f0e2c2e76e9953e1172279f0ad41
otherotherjosh <itsotherjosh@gmail.com>
2025-11-15 13:54:56 +1300

    feat: add count in metronome

--- beatmaps/beatmap_scene.gd
+++ beatmaps/beatmap_scene.gd
@@ -3,9 +3,17 @@ extends Node2D
 
 @export var beatmap: Beatmap
 
+@onready var metronome: AudioStreamPlayer = $Metronome
+
 
 func _ready() -> void:
-	BeatmapPlayer.beatmap = beatmap
-	BeatmapPlayer.queue_beatmap()
 	TouchInputManager.screen_touch_pressed.connect(func(pos):
 		BeatmapPlayer.attempt_hit_target())
+	MusicPlayer.on_count_in.connect(func(count):
+		metronome.pitch_scale = 1.5 if count % 4 == 0 else 1
+		metronome.play())
+		
+	await get_tree().create_timer(1).timeout
+	
+	BeatmapPlayer.beatmap = beatmap
+	BeatmapPlayer.queue_beatmap()
