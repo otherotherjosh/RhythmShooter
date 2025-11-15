7694a283e721a4033d7ceaa290c51bb84d85c527
otherotherjosh <itsotherjosh@gmail.com>
2025-11-16 12:41:22 +1300

    feat: add intro scene

---
+++ scenes/intro/intro_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+@export var labels: Array[RichTextLabel]
+
+
+func _ready() -> void:
+	for label in labels:
+		label.visible = false
+	await get_tree().create_timer(1).timeout
+	for label in labels:
+		label.visible = true
+		await get_tree().create_timer(2).timeout
