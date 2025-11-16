a23d6612873f5057ed20575deeefeeefd895cf83
otherotherjosh <itsotherjosh@gmail.com>
2025-11-16 15:05:54 +1300

    feat: add skip button to intro

--- scenes/intro/intro_scene.gd
+++ scenes/intro/intro_scene.gd
@@ -3,8 +3,14 @@ extends Node2D
 
 @export var labels: Array[RichTextLabel]
 
+var skip_taps := 0
+
+@onready var skip_hint: RichTextLabel = $CanvasLayer/SkipHint
+
 
 func _ready() -> void:
+	TouchInputManager.screen_touch_pressed.connect(skip_tap)
+	skip_hint.visible = false
 	for label in labels:
 		label.visible = false
 	await get_tree().create_timer(1).timeout
@@ -12,3 +18,12 @@ func _ready() -> void:
 		label.visible = true
 		await get_tree().create_timer(2).timeout
 	Global.load_level(0)
+
+
+func skip_tap(_pos) -> void:
+	skip_taps += 1
+	match skip_taps:
+		2:
+			skip_hint.visible = true
+		3:
+			Global.load_level(0)
