5ba0fb35cfb449ef5dd294b888cd37625c2a4d2a
otherotherjosh <itsotherjosh@gmail.com>
2025-11-16 14:08:27 +1300

    feat: add save dictionary

--- global/global.gd
+++ global/global.gd
@@ -3,3 +3,19 @@ extends Node
 
 const SCENE_MENU := preload("res://scenes/menu/main_menu.tscn")
 const SCENE_INTRO := preload("res://scenes/intro/intro.tscn")
+const SAVE_DATA_PATH := "user://save_data.json"
+
+var save: Dictionary
+
+
+func _ready() -> void:
+	if not FileAccess.file_exists(SAVE_DATA_PATH):
+		init_save_data()
+		return
+	# load save data
+
+
+func init_save_data() -> void:
+	save = {
+		"first-time" : true,
+	}
