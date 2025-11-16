b0db91b6191bcd3a91ee5e03403561a88878b5ae
otherotherjosh <itsotherjosh@gmail.com>
2025-11-16 14:34:14 +1300

    feat: export save to file

--- global/global.gd
+++ global/global.gd
@@ -5,17 +5,43 @@ const SCENE_MENU := preload("res://scenes/menu/main_menu.tscn")
 const SCENE_INTRO := preload("res://scenes/intro/intro.tscn")
 const SAVE_DATA_PATH := "user://save_data.json"
 
-var save: Dictionary
+var save: Dictionary:
+	set = _set_save
 
 
 func _ready() -> void:
 	if not FileAccess.file_exists(SAVE_DATA_PATH):
 		init_save_data()
 		return
-	# load save data
+	load_save_data()
 
 
 func init_save_data() -> void:
 	save = {
-		"first-time" : true,
+		"first_time" : true,
 	}
+	print("that was init")
+
+
+func load_save_data() -> void:
+	var save_file: JSON = load(SAVE_DATA_PATH)
+	save = save_file.data
+	print("loaded")
+
+
+func write_to_save(key: String, value: Variant) -> void:
+	save[key] = value
+	export_save_file()
+
+
+func export_save_file() -> void:
+	var file := FileAccess.open(SAVE_DATA_PATH, FileAccess.WRITE)
+	var json_string = JSON.stringify(save)
+	file.store_line(json_string)
+	file.close()
+
+
+func _set_save(value: Dictionary) -> void:
+	if save == value: return
+	save = value
+	export_save_file()
--- scenes/init.gd
+++ scenes/init.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 
 func _ready() -> void:
-	get_tree().change_scene_to_packed(Global.SCENE_MENU)
+	await get_tree().process_frame
+	var scene = Global.SCENE_INTRO if Global.save["first_time"] else Global.SCENE_MENU
+	Global.write_to_save("first_time", false)
+	get_tree().change_scene_to_packed(scene)
