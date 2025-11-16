8e7a9a75f092b0c57bace24b16f9cf049c262122
otherotherjosh <itsotherjosh@gmail.com>
2025-11-16 15:33:13 +1300

    feat: add controls to level

--- scenes/level 0/level_0.gd
+++ scenes/level 0/level_0.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 
 @export var events: Array[Resource]
+
+@onready var beat_target_circle: BeatTargetCircle = $Parallax2D/BeatTargetCircle
+
+
+func _ready() -> void:
+	beat_target_circle.global_position.x = 0
+	beat_target_circle.shader_material.set_shader_parameter("alpha", 0.3)
